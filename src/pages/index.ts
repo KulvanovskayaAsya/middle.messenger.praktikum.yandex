7c0e0a9aaac9b2083ffc1588b23a180708878a2c
KulvanovskayaAsya <nastena.kulvanovskaya.02@mail.ru>
2024-03-18 12:41:24 +0300

    add pages

---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+export { LoginPage } from './login';
+export { RegistrationPage } from './registration';
+
+export { ChatPage } from './chat';
+
+export { ErrorPage } from './error';
+
+export { ProfilePage } from './profile';
+export { ChangePasswordPage } from './change-password';
