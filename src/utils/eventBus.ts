26573f4c5582d865d82b7deb472fdea20dd47dba
KulvanovskayaAsya <nastena.kulvanovskaya.02@mail.ru>
2024-03-26 01:01:07 +0300

    Create eventBus.ts

---
+++ src/utils/eventBus.ts
@@ -0,0 +1,38 @@
+type Listener = (...args: unknown[]) => void;
+
+class EventBus {
+	private listeners: { [event: string]: Listener[] };
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event: string, callback: Listener): void {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(callback);
+  }
+
+  off(event: string, callback: Listener): void {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event].filter(
+      (listener) => listener !== callback
+    );
+  }
+
+  emit(event: string, ...args: unknown[]): void {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(...args);
+    });
+  }
+}
+
+export default EventBus;
