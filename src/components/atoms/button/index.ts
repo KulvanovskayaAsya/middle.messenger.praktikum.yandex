91c835dfe4dd5b9c7503b485e03fb2346a41b237
Ася Кульвановская <kulvanovskaya.aa@prj365.com>
2024-03-27 17:16:12 +0300

    attempt to implement button class

--- src/components/atoms/button/index.ts
+++ src/components/atoms/button/index.ts
@@ -1,3 +1,29 @@
-import './button.scss';
+import Block from "../../../utils/block";
+import "./button.scss";
+import template from "./button.hbs?raw";
 
-export { default as Button } from './button.hbs?raw';
+interface ButtonProps {
+  text?: string;
+  icon?: string;
+  additionalClasses?: string;
+  hrefPage?: string;
+  onClick?: () => void;
+}
+
+class Button extends Block {
+  constructor(props: ButtonProps) {
+    super("button", { 
+			...props, 
+			events: { 
+				click: props.onClick
+			} 
+		});
+  }
+
+  render(): string {
+    const temp = Handlebars.compile(template);
+    return temp(this.props);
+  }
+}
+
+export default Button;
