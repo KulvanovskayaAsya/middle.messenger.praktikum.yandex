8343177c831445c9be3b6352e642797d063d9504
KulvanovskayaAsya <nastena.kulvanovskaya.02@mail.ru>
2024-03-29 01:12:40 +0300

    login and registration pages

--- src/pages/login/index.ts
+++ src/pages/login/index.ts
@@ -1,3 +1,61 @@
 import './login.scss';
+import template from './login.hbs?raw';
+import BaseComponent from '../../utils/base-component';
+import Form from '../../components/organisms/form';
+import TextField from '../../components/molecules/text-field';
+import Button from '../../components/atoms/button';
+import Link from '../../components/atoms/link';
+import PageTitle from '../../components/atoms/page-title';
 
-export { default as LoginPage } from './login.hbs?raw';
+import { authenticationForm } from '../../utils/mock-data';
+
+interface ILoginPageProps {
+  
+}
+
+const fields = authenticationForm.map(field => new TextField({
+  input: {
+    id: field.id,
+    name: field.name,
+    inputType: field.type
+  },
+  label: {
+    id: field.id,
+    label: field.label
+  }
+}));
+
+const submitButton = new Button({
+  text: 'Войти',
+  additionalClasses: 'button_primary',
+  hrefPage: 'chatPage',
+  onClick: () => {
+    console.log('Форма отправлена');
+  }
+});
+
+class LoginPage extends BaseComponent {
+  constructor(props: ILoginPageProps) {
+    super({ 
+      ...props,
+      pageTitle: new PageTitle({
+        text: 'Вход'
+      }),
+      form: new Form({
+        textFields: fields,
+        button: submitButton
+      }),
+      link: new Link({
+        link: '#',
+        text: 'Нет аккаунта?',
+        hrefPage: 'registrationPage'
+      })
+    });
+  }
+
+  render() {
+    return this.compile(template, this.props);
+  }
+}
+
+export default LoginPage;
--- src/pages/registration/index.ts
+++ src/pages/registration/index.ts
@@ -1,3 +1,62 @@
-import './registration.scss';
+import './registration.scss'; // Предполагаем, что у вас есть свои стили
+import template from './registration.hbs?raw';
+import BaseComponent from '../../utils/base-component';
+import Form from '../../components/organisms/form';
+import TextField from '../../components/molecules/text-field';
+import Button from '../../components/atoms/button';
+import Link from '../../components/atoms/link';
+import PageTitle from '../../components/atoms/page-title';
 
-export { default as RegistrationPage } from './registration.hbs?raw';
+import { registrationForm } from '../../utils/mock-data';
+
+interface IRegistrationPageProps {
+  // Можно добавить свойства, если они потребуются
+}
+
+// Определение полей для формы регистрации
+const fields = registrationForm.map(field => new TextField({
+  input: {
+    id: field.id,
+    name: field.name,
+    inputType: field.type
+  },
+  label: {
+    id: field.id,
+    label: field.label
+  }
+}));
+
+const submitButton = new Button({
+  text: 'Зарегистрироваться',
+  hrefPage: 'chatPage',
+  additionalClasses: 'button_primary',
+  onClick: () => {
+    console.log('Форма регистрации отправлена');
+  }
+});
+
+class RegistrationPage extends BaseComponent {
+  constructor(props: IRegistrationPageProps) {
+    super({
+      ...props,
+      pageTitle: new PageTitle({
+        text: 'Регистрация'
+      }),
+      form: new Form({
+        textFields: fields,
+        button: submitButton
+      }),
+      link: new Link({
+        link: '#',
+        text: 'Есть аккаунт?',
+        hrefPage: 'loginPage'
+      })
+    });
+  }
+
+  render() {
+    return this.compile(template, this.props);
+  }
+}
+
+export default RegistrationPage;
