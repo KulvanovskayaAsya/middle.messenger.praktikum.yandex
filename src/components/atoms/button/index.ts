847bc40629208d000a6ac6a8d26f270894915e63
KulvanovskayaAsya <nastena.kulvanovskaya.02@mail.ru>
2024-03-30 18:15:12 +0300

    try to switch button activity

--- src/components/atoms/button/index.ts
+++ src/components/atoms/button/index.ts
@@ -1,34 +1,15 @@
 import BaseComponent from '../../../utils/base-component';
 import './button.scss';
 import template from './button.hbs?raw';
+import validate from '../../../utils/validation';
 
 export interface IButtonProps {
   text?: string;
   icon?: string;
   additionalClasses?: string;
   hrefPage?: string;
+  isDisabled?: boolean;
   events?: Record<string, (e: Event) => void>;
-}
-
-function grabFormValues(e: Event): void {
-  const buttonElement = e.currentTarget as HTMLElement;
-  const form = buttonElement.closest('form');
-
-  if (form) {
-    const formData: Record<string, string> = {};
-    const formElements = form.elements;
-    
-    for (let i = 0; i < formElements.length; i++) {
-      const element = formElements[i] as HTMLInputElement;
-      if (element.name) {
-        formData[element.name] = element.value;
-      }
-    }
-    
-    console.log(formData);
-  } else {
-    console.log('Форма не найдена');
-  }
 }
 
 class Button extends BaseComponent {
@@ -36,13 +17,51 @@ class Button extends BaseComponent {
     super({
       ...props,
       events: {
-        click: (e: Event) => grabFormValues(e)
+        click: (event: Event) => this.handleFormSubmit(event)
       }
     });
   }
 
   render(): HTMLElement {
     return this.compile(template, this.props);
+  }
+
+  grabFormValues(form: HTMLFormElement): void {
+    const formData: Record<string, string> = {};
+    const formElements = form.elements;
+    let isValidForm = false;
+    
+    for (let i = 0; i < formElements.length; i++) {
+      const element = formElements[i] as HTMLInputElement;
+      if (element.name) {
+        formData[element.name] = element.value;
+        const isValidField = validate(formData[element.name], element.value).isValid;
+        if(!isValidField)
+          break;
+      }
+    }
+
+    if(!isValidForm) {
+      this.setProps({
+        ...this.props,
+        isDesabled: true
+      })
+    }
+
+    console.log(this.props)
+    
+    console.log(formData);
+  }
+
+  handleFormSubmit(e: Event): void {
+    const buttonElement = e.currentTarget as HTMLElement;
+    const form = buttonElement.closest('form');
+  
+    if (form) {
+      this.grabFormValues(form);
+    } else {
+      console.log('Форма не найдена');
+    }
   }
 }
 
