6cefec2727b854949df62dd21d93d7486911c283
KulvanovskayaAsya <nastena.kulvanovskaya.02@mail.ru>
2024-04-23 23:54:42 +0300

    update profile service

--- src/services/profile-service/index.ts
+++ src/services/profile-service/index.ts
@@ -9,20 +9,43 @@ class ProfileService {
     return store.getState();
   }
 
-  public async editProfile(data: ProfileData) {
+  public async changeProfile(data: ProfileInfo) {
     try {
-      const response = await this.API.changeProfile(data);
-      store.setState('profileInfo', {
-        response
-      });
-
-      alert("Профиль успешно обновлен");
-      return response;
+      const profileData = JSON.parse(await this.API.changeProfile(data) as string);
+      
+      this._setStoreProfileInfo(profileData);
     } catch (error) {
-      console.error("Ошибка обновления профиля:", error);
-      alert("Ошибка обновления профиля: " + error);
-      return null;
+      alert('Ошибка обновления профиля: ' + error);
     }
+  }
+
+  public async changeAvatar(avatarFile: File) {
+    try {
+      const profileData = await JSON.parse(await this.API.changeAvatar(avatarFile) as string);
+
+      this._setStoreProfileInfo(profileData);
+    } catch (error) {
+      alert('Ошибка обновления аватара профиля: ' + error);
+    }
+  }
+
+  public async changePassword(oldPassword: string, newPassword: string) {
+    try {
+      const dataForAPI = {
+        oldPassword: oldPassword,
+        newPassword: newPassword
+      };
+
+      await this.API.changePassword(dataForAPI);
+    } catch (error) {
+      alert('Ошибка обновления пароля: ' + error);
+    }
+  }
+
+  private async _setStoreProfileInfo(profileInfo: ProfileInfo) {
+    store.setState('profileInfo', {
+      ...profileInfo
+    });
   }
 }
 
